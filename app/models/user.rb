8e31348d562d247fe6fe64d3d856d4588c61c0fb
ono775 <ono7751986@gmail.com>
2021-01-17 00:07:36 +0900

    バリデーションの設定
    email 一意性
    password 英数字混在

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :schedules
+  has_many :objects
+
+  with_options presence: true do
+    validates :name
+    validates :email, uniqueness: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  end
 end
