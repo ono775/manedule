7599c3b35ba259dcf623d5f22d08553a8a5e75b0
ono775 <ono7751986@gmail.com>
2021-01-20 20:46:42 +0900

    コントローラーの追記

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,26 @@
 class SchedulesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  
   def index
+    @schedules = Schedule.all
+  end
+
+  def new
+    @schedule = Schedule.new
+  end
+
+  def create
+    @schedule = Schedule.new(schedule_params)
+    if @schedule.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:start_time, :client, :title, :content).merge(user_id: current_user.id)
   end
 end
