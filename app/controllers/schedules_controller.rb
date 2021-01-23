3153292398c04e66af50fbbe59b4d9c67f5f938a
ono775 <ono7751986@gmail.com>
2021-01-23 18:01:55 +0900

    編集ページへの遷移

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,5 +1,6 @@
 class SchedulesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :set_schedule, only: [:show, :edit, :update]
 
   def index
     @schedules = Schedule.all
@@ -19,15 +20,26 @@ class SchedulesController < ApplicationController
   end
 
   def show
-    @schedule = Schedule.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @schedule.update(schedule_params)
+      redirect_to schedule_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def schedule_params
     params.require(:schedule).permit(:start_date, :end_date, :client, :title, :content).merge(user_id: current_user.id)
+  end
+
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
   end
 end
