75e1d7d8e7af293e5bb3311b7344e110b4291637
ono775 <ono7751986@gmail.com>
2021-01-25 15:47:47 +0900

    Item モデルの生成
    モデル名表記の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :schedules
-  has_many :objects
+  has_many :items
 
   with_options presence: true do
     validates :name
