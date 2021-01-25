73ad702b0fd942fc7bc78f0efa19ff3b82e90917
ono775 <ono7751986@gmail.com>
2021-01-25 23:00:40 +0900

    在庫作成の実装
    バリデーションの修正
    カラムの修正

--- db/migrate/20210125064232_create_items.rb
+++ db/migrate/20210125064232_create_items.rb
@@ -2,9 +2,8 @@ class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
       t.integer    :item_name_id,   null: false
-      t.integer    :status_id, null: false
-      t.references :user,      null: false, foreign_key: true
-      t.references :schedule,  null: false, foreign_key: true
+      t.integer    :status_id,      null: false
+      t.references :user,           null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,8 @@ ActiveRecord::Schema.define(version: 2021_01_25_064232) do
     t.integer "item_name_id", null: false
     t.integer "status_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "schedule_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["schedule_id"], name: "index_items_on_schedule_id"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
@@ -48,7 +46,6 @@ ActiveRecord::Schema.define(version: 2021_01_25_064232) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "items", "schedules"
   add_foreign_key "items", "users"
   add_foreign_key "schedules", "users"
 end
