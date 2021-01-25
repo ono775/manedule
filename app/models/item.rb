603d9659c703ce8dcd645193cdbc4b7bbc010907
ono775 <ono7751986@gmail.com>
2021-01-25 17:19:43 +0900

    item_name, status モデルの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,12 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :schedule
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_name
+  belongs_to_active_hash :status
+
   with_options presence: true do
-    validates :item_name_id
-    validates :status_id
+    validates :item_name_id, numericality: { other_than: 0 }
+    validates :status_id, numericality: { other_than: 0 }
   end
 end
