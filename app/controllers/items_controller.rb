d96d0ff571f1a9b9a7d509c9ccbb14b0fc027812
ono775 <ono7751986@gmail.com>
2021-03-14 16:13:25 +0900

    機材編集ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+
   def index
     @items = Item.all.order(:item_name_id)
   end
@@ -21,10 +23,17 @@ class ItemsController < ApplicationController
     @schedules = Schedule.all
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:item_name_id, :status_id).merge(user_id: current_user.id)
   end
 
+  def set_items
+    @item = Item.find(params[:id])
+  end
 end
