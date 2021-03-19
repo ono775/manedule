01d447ed598e7c675be92399a68f6ed892994477
ono775 <ono7751986@gmail.com>
2021-03-19 22:52:45 +0900

    カラムの追加
    アソシエーションの指定

--- db/migrate/20210319134424_create_schedule_members.rb
+++ db/migrate/20210319134424_create_schedule_members.rb
@@ -1,7 +1,8 @@
 class CreateScheduleMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :schedule_members do |t|
-
+      t.integer :schedule_id
+      t.integer :member_id
       t.timestamps
     end
   end
