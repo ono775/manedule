f56f9b4c0643b34c53665dc102ecbda3514252d6
ono775 <ono7751986@gmail.com>
2021-03-19 13:36:19 +0900

    rubpcop による修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,4 @@
 class Member < ApplicationRecord
-
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :name
   belongs_to_active_hash :position
--- spec/requests/members_request_spec.rb
+++ spec/requests/members_request_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
-RSpec.describe "Members", type: :request do
-
+RSpec.describe 'Members', type: :request do
 end
