a796b1561b8461878f81240596c63630d94f3521
ono775 <ono7751986@gmail.com>
2021-03-19 13:36:56 +0900

    member 詳細ページへの遷移

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,11 @@
 class MembersController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_members, only: [:show]
 
   def index
     @members = Member.all.order(:name_id)
   end
-  
+
   def new
     @member = Member.new
   end
@@ -18,9 +19,17 @@ class MembersController < ApplicationController
     end
   end
 
+  def show
+    @schedules = Schedule.all
+  end
+
   private
 
   def member_params
     params.require(:member).permit(:name_id, :position_id)
+  end
+
+  def set_members
+    @member = Member.find(params[:id])
   end
 end
